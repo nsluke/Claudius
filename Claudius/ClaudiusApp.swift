d7d3870a7b26e9eaa2a4580ec03ed9576130aad2
NSLuke <solomora@gmail.com>
2026-03-10 15:35:45 -0400

    Initial Commit

---
+++ Claudius/ClaudiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudiusApp.swift
+//  Claudius
+//
+//  Created by Luke Solomon on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
